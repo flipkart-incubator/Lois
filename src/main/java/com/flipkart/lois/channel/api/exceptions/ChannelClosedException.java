4c1af7055d2bb86fd4b6e708f5caa4e36bb18ba2
suman.karthik <suman.karthik@flipkart.com>
2014-04-12 06:43:50 +0530

    Created a basic Channle interface

---
+++ src/main/java/com/flipkart/lois/channel/api/exceptions/ChannelClosedException.java
@@ -0,0 +1,23 @@
+package com.flipkart.lois.channel.api.exceptions;
+
+/**
+ * This is thrown when invalid operations on Channel are executed after it has been closed
+ */
+
+public class ChannelClosedException extends Exception{
+    public ChannelClosedException() {
+        super();
+    }
+
+    public ChannelClosedException(String message) {
+        super(message);
+    }
+
+    public ChannelClosedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ChannelClosedException(Throwable cause) {
+        super(cause);
+    }
+}
